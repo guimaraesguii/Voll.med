63f3d81a271516d334046f05391001bcb1acf92f
guimaraesguii <guifereler@gmail.com>
2024-07-29 04:19:07 -0300

    register patients

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,55 @@
+package med.voll.api.paciente;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosAtualizacaoMedicos;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    private Boolean ativo;
+
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+        this.telefone = dados.telefone();
+        this.ativo = true;
+    }
+    public void atualizarInformacoes(DadosAtualizacaoMedicos dados){
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+}
+
